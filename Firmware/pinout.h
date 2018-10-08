3428b7e2b5c6bf6b5b06a6f2c8f0d4e1d21b66ff
LeonardoSanBenitez <31520774+LeonardoSanBenitez@users.noreply.github.com>
2018-10-08 12:39:37 -0300

    ajeitando pequenas más práticas

---
+++ Firmware/pinout.h
@@ -0,0 +1,12 @@
+//Pin definitions
+#define INFRA_1 A1
+#define INFRA_2 A2
+#define INFRA_3 A3
+
+#define BUZZER  22
+#define POT     A0
+#define MOTOR_A 10
+#define MOTOR_B 11
+
+#define pino_trigger 12 //sensor ultrassonico: (trigger no pino digital 7)
+#define pino_echo 13    //sensor ultrassonico: (echo no pino digital 8)
